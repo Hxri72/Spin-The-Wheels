d5e6e828dcdc6e3c7c451c2cd3699a3a67cc7a66
Hariprasad K H <hariprasad72172@gmail.com>
2022-12-01 19:24:28 +0530

    mongoose bugs solved

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose')
+require("dotenv").config();
 
-mongoose.connect('mongodb+srv://Hariprasad7217:Ad6LtKGLHBb8rRTs@cluster0.xcqajze.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true});
+
+mongoose.connect(process.env.MONGO_URI,{useNewUrlParser:true});
 
 
 mongoose.connection
