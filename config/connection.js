6c329639c7863fe2073b0f2755aa94b53624f5a2
Hariprasad K H <hariprasad72172@gmail.com>
2022-12-01 17:14:42 +0530

    mongoos

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 
-
 mongoose.connect('mongodb+srv://Hariprasad7217:Ad6LtKGLHBb8rRTs@cluster0.xcqajze.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true});
 
 
