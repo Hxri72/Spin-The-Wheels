3c3d1df28e2e7b532b8170fe6f9ee61aa2ab28cf
Hariprasad K H <hariprasad72172@gmail.com>
2022-12-01 10:55:29 +0530

    Mongoos connected

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/SpinTheWheels',{useNewUrlParser:true});
+mongoose.connect('mongodb+srv://Hariprasad7217:Ad6LtKGLHBb8rRTs@cluster0.xcqajze.mongodb.net/?retryWrites=true&w=majority',{useNewUrlParser:true});
 
 
 mongoose.connection
